a4bdbb4e5f1b89131fe99545e17c2eac280506b3
balaji-githubstore <balaji0017@gmail.com>
2023-03-16 15:29:11 +0530

    Created Test method for valid login

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,19 +1,26 @@
-import pytest
-from selenium import webdriver
 from assertpy import assert_that
 from selenium.webdriver.common.by import By
 
+from base.webdriver_listner import WebDriverWrapper
 
-class TestLoginUI:
-    @pytest.fixture(scope="function",autouse=True)
-    def browser_config(self):
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-        self.driver.implicitly_wait(20)
-        self.driver.get('https://opensource-demo.orangehrmlive.com/')
-        yield
-        self.driver.quit()
 
+class TestLogin(WebDriverWrapper):
+    def test_valid_login(self):
+        self.driver.find_element(By.NAME, "username").send_keys("Admin")
+        self.driver.find_element(By.NAME, "password").send_keys("admin123")
+        self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+        actual_header = self.driver.find_element(By.XPATH, "//h6[contains(normalize-space(),'Dash')]").text
+        assert_that('Dashboard').is_equal_to(actual_header)
+
+    def test_invalid_login(self):
+        self.driver.find_element(By.NAME, "username").send_keys("john")
+        self.driver.find_element(By.NAME, "password").send_keys("john123")
+        self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+        # Assert the error message - Invalid credentials
+        #will start at 3:45 PM IST
+
+
+class TestLoginUI(WebDriverWrapper):
     def test_title(self):
         actual_title = self.driver.title
         assert_that('OrangeHRM').is_equal_to(actual_title)
@@ -25,5 +32,5 @@ class TestLoginUI:
         assert_that('Password').is_equal_to(actual_password_placehodler)
 
     def test_header(self):
-        actual_header=self.driver.find_element(By.XPATH,"//h5").text
+        actual_header = self.driver.find_element(By.XPATH, "//h5").text
         assert_that('Login').is_equal_to(actual_header)
