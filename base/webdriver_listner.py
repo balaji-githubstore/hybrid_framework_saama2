1705c090748e8790b2f8633f9e696c2b706b2ecf
balaji-githubstore <balaji0017@gmail.com>
2023-03-23 14:20:25 +0530

    Cross browser code added

--- base/webdriver_listner.py
+++ base/webdriver_listner.py
@@ -7,7 +7,15 @@ class WebDriverWrapper:
 
     @pytest.fixture(scope="function", autouse=True)
     def browser_config(self):
-        self.driver = webdriver.Edge()
+        browser_name="ch"
+
+        if browser_name=="edge":
+            self.driver = webdriver.Edge()
+        elif browser_name=="ff":
+            self.driver=webdriver.Firefox()
+        else:
+            self.driver=webdriver.Chrome()
+
         self.driver.maximize_window()
         self.driver.implicitly_wait(20)
         self.driver.get('https://opensource-demo.orangehrmlive.com/')
