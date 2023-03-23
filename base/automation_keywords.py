38dd94ed5c47e79588c450f2ac2cbba816c67afb
balaji-githubstore <balaji0017@gmail.com>
2023-03-23 16:00:27 +0530

    Updated the keyword. Keyword used through inheritance by page objects

--- base/automation_keywords.py
+++ base/automation_keywords.py
@@ -8,16 +8,16 @@ class AutomationKeywords:
         self._wait = WebDriverWait(driver, 30)
 
     def type_by_locator(self, locator, text):
-        self._wait.until(expected_conditions.visibility_of_element_located((locator))).send_keys(text)
+        self._wait.until(expected_conditions.visibility_of_element_located(locator)).send_keys(text)
 
     def click_by_locator(self, locator):
-        self._wait.until(expected_conditions.visibility_of_element_located((locator))).click()
+        self._wait.until(expected_conditions.visibility_of_element_located(locator)).click()
 
     def get_text_by_locator(self, locator):
-        return self._wait.until(expected_conditions.visibility_of_element_located((locator))).text
+        return self._wait.until(expected_conditions.visibility_of_element_located(locator)).text
 
     def get_attribute_by_locator(self, locator, attribute):
-        return self._wait.until(expected_conditions.visibility_of_element_located((locator))).get_attribute(attribute)
+        return self._wait.until(expected_conditions.visibility_of_element_located(locator)).get_attribute(attribute)
 
     def switch_tab_by_title(self, title):
         for window in self._driver.window_handles:
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,12 @@
 from selenium.webdriver.common.by import By
+from base.automation_keywords import AutomationKeywords
 
 
-class Mainpage:
+class Mainpage(AutomationKeywords):
     def __init__(self, driver):
-        self.driver = driver
+        super().__init__(driver)
+        # self.driver = driver
+        self.__pim_menu_locator = (By.XPATH, "//span[normalize-space()='PIM']")
 
     def click_on_pim_menu(self):
-        self.driver.find_element(By.XPATH, "//span[normalize-space()='PIM']").click()
+        self.click_by_locator(self.__pim_menu_locator)
